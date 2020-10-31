4d40008fe79d4e1337c4dc9b93bd6913ae18a3f3
unknown <aaronbarcenasm@gmail.com>
2020-10-30 19:04:00 -0600

    Subir primeros cambios de la clase 04

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -7,11 +7,43 @@ class Counter extends Component {
     this.state = {
       counter: 0
     }
+    console.log("Constructor")
+  }
+
+  componentDidMount() {
+    console.log("Componentte montado ComponentDidMount()")
+    this.setState({
+      counter: 1
+    })
+  }
+
+  componentDidUpdate() {
+    console.log("El componente se actualizo")
+  }
+
+  clickAddHandler() {
+    let { counter } = this.state
+    this.setState({
+      counter: counter + 1
+    })
+  }
+
+  clickRestHandler() {
+    let { counter } = this.state
+    this.setState({
+      counter: counter - 1
+    })
   }
 
   render() {
+    console.log("Render", this.state.counter)
     return (
-    <h1>Counter: {this.state.counter}</h1>
+    <div>
+      <h1>Counter: {this.state.counter}</h1>
+      <button onClick={() => this.clickAddHandler()}>Add</button>
+      <button onClick={() => this.clickRestHandler()}>Rest</button>
+    </div>
+    
     )
   }
 }
